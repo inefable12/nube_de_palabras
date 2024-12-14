f6b7c0bbec57237f020c762fcd809a343fedbc3e
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-12-14 17:11:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,37 @@ import streamlit as st
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from io import StringIO
+import string
+from nltk.corpus import stopwords
+
+# Cargar stopwords en español
+stop_words = set(stopwords.words('spanish'))
+
+# Función para limpiar puntuación y stopwords
+def limpiar_puntuacion_stopwords(texto):
+    puntuacion = []
+    for s in string.punctuation:
+        puntuacion.append(str(s))
+    sp_puntuacion = ["¿", "¡", "“", "”", "…", ":", "–", "»", "«", "?", "!"]
+    puntuacion += sp_puntuacion
+
+    # Reemplazamos signos de puntuación por ""
+    for p in puntuacion:
+        texto = texto.lower().replace(p, "")
+
+    # Reemplazamos stop_words por ""
+    texto_lista = texto.split()
+    texto_lista = [i.strip() for i in texto_lista]
+    try:
+        while stop in texto_lista:
+            texto_lista.remove(stop)
+    except:
+        print("Error")
+        pass
+
+    texto_limpio = " ".join(texto_lista)
+
+    return texto_limpio
 
 # Título de la app
 st.title("Generador de Nube de Palabras")
@@ -19,17 +50,16 @@ if uploaded_file is not None:
     # Leer el archivo
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     text = stringio.read()
-    
-    # Mostrar el texto cargado (opcional)
-    ##st.subheader("Texto cargado:")
-    ##st.write(text[:500])  # Muestra los primeros 500 caracteres
 
+    # Limpiar el texto cargado
+    text_limpio = limpiar_puntuacion_stopwords(text)
+    
     # Generar la nube de palabras con el color de fondo seleccionado
     wordcloud = WordCloud(
         width=800,
         height=400,
         background_color=background_color
-    ).generate(text)
+    ).generate(text_limpio)
 
     # Mostrar la nube de palabras
     st.subheader("Nube de Palabras Generada")
