b81aa532986cf0e9d80ac83adbad5e8bb9a9a6d3
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-12-14 17:00:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,16 +9,11 @@ st.title("Generador de Nube de Palabras")
 # Subir archivo de texto
 uploaded_file = st.file_uploader("Sube un archivo de texto", type="txt")
 
-# Opciones para seleccionar color de fondo y estilo de las letras
+# Opciones para seleccionar color de fondo
 background_color = st.selectbox(
     "Selecciona el color de fondo", 
     ["white", "black", "blue", "yellow", "green", "red"]
 )
-
-#font_style = st.selectbox(
-#    "Selecciona el estilo de letra", 
-#    ["arial", "times new roman", "courier", "helvetica", "comic sans ms"]
-#)
 
 if uploaded_file is not None:
     # Leer el archivo
@@ -29,12 +24,11 @@ if uploaded_file is not None:
     ##st.subheader("Texto cargado:")
     ##st.write(text[:500])  # Muestra los primeros 500 caracteres
 
-    # Generar la nube de palabras con las opciones seleccionadas
+    # Generar la nube de palabras con el color de fondo seleccionado
     wordcloud = WordCloud(
         width=800,
         height=400,
-        background_color=background_color,
-        font_path=None if font_style == "arial" else f"/usr/share/fonts/truetype/msttcorefonts/{font_style}.ttf"
+        background_color=background_color
     ).generate(text)
 
     # Mostrar la nube de palabras
